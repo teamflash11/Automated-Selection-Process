c53a3c60fcec03a8b383c432fba63025db89fe01
Maulik003 <joshimaulikp@gmail.com>
2018-03-26 21:30:40 +0530

    changes

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <?php 
 include ("connect.php");  
 include('PdfToText-master/PdfToText.phpclass');
-$pdf=new PdfToText('http://localhost/GitHub/Automated-Selection-Process/tset1.pdf');
+$pdf=new PdfToText('http://localhost/GitHub/Automated-Selection-Process/Sonu.pdf');
 $string1 = "C";
 $string2 = "Java";
 $string3 = "Python";
+$var1=$var2=$var3="false";
 $data = $pdf->Text;
 if(strpos($data,$string1)!== false){
 	echo $string1;
-	$sql = "INSERT INTO languages (C,Java,Python)
-    VALUES ('true','','')";
-	$conn->query($sql);
+	$var1="true";
+	
 
 }
 if(strpos($data,$string2)!== false){
-	echo $string1;
-	$sql = "INSERT INTO languages (C,Java,Python)
-    VALUES ('','true','')";
-	$conn->query($sql);
-
+	echo $string2;
+	$var2="true";
 }
 if(strpos($data,$string3)!== false){
-	echo $string1;
-	$sql = "INSERT INTO languages (C,Java,Python)
-    VALUES ('','','true')";
-	$conn->query($sql);
+	echo $string3;
+	$var3="true";
+	
 
 }
-
+$sql = "INSERT INTO languages (C,Java,Python)
+    VALUES ($var1,$var2,$var3)";
+	$conn->query($sql);
+	
+	
 mysqli_close($conn);
 ?>
