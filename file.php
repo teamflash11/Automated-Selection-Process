4dddfd3f9e29dc8e1e725e5c4e11a8c57fcb940a
dishti <dishtidave@gmail.com>
2018-03-30 18:28:39 +0530

    create uploads folder in htdocs

---
+++ file.php
@@ -0,0 +1,7 @@
+
+<form action="uploads1.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="file" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+
