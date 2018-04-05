32756470640771c6843c88b73d59eb0267771dcb
dishti <dishtidave@gmail.com>
2018-04-05 12:50:48 +0530

    changes now

---
+++ header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+	<head>
+		<title></title>
+	<!--	<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" /> -->
+		
+	</head>
+	<body>
+
+<img src="images/i1.gif" top-margin-top="0" width="100%" height="100"></img>
+				
+			</body>
+			</html>
