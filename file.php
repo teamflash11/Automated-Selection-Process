89b899a1b888c74c845900562e5884de3b4b09cb
Maulik003 <joshimaulikp@gmail.com>
2018-05-01 16:38:04 +0530

    changes

--- file.php
+++ file.php
@@ -1,7 +1,10 @@
+<html>
 
+<body bgcolor="#00ffff">
 <form action="uploads1.php" method="post" enctype="multipart/form-data">
     Select File to upload:
     <input type="file" name="file" id="fileToUpload">
     <input type="submit" value="Upload File" name="submit">
 </form>
-
+</body>
+</html>
